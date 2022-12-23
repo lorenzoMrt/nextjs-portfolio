e7efc54e57d5e6275a6dbcaec5bd208a9faaba1b
lorenzo miret <lmiret@biobam.com>
2022-12-23 12:41:02 +0100

    mdx setup

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,5 +1,50 @@
-import { Heading } from "@chakra-ui/react";
+import {
+  Heading,
+  LinkBox,
+  LinkOverlay,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import getLocalMdx from "@components/utils/getLocalMdx";
+import Link from "next/link";
 
-export default function Posts() {
-  return <Heading as="h1">Posts Page</Heading>;
+export default function BlogPage({ posts }: { posts: any }) {
+  return (
+    <Stack spacing={[4, 8, 12]} justify="center" alignItems="center">
+      {posts.map((post: any) => (
+        <LinkBox
+          mt={6}
+          p={8}
+          border="1px solid black"
+          borderRadius="8px"
+          as="article"
+          key={post.slug}
+        >
+          <VStack>
+            <Link href={`/posts/${post.slug}`} passHref>
+              <LinkOverlay>
+                <Heading>{post.title}</Heading>
+              </LinkOverlay>
+            </Link>
+            <Text>{post.description}</Text>
+          </VStack>
+        </LinkBox>
+      ))}
+    </Stack>
+  );
+}
+
+export async function getStaticProps() {
+  const posts = await getLocalMdx("./posts");
+  const allMdx = posts.map((post) => ({
+    slug: post.slug,
+    ...post.data,
+  }));
+
+  return {
+    props: {
+      posts: allMdx,
+    },
+  };
 }
